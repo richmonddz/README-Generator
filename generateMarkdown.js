6564819a4cceb9e8058dd11b21f5486172520570
Richmond Dominguez <richmonddominguez@gmail.com>
2022-05-20 22:04:11 -0700

    added to the genmarkdown file

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,6 +1,8 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  return `![Badge](https://img.shields.io/badge/License-${license}-blueviolet)`;
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -13,6 +15,23 @@ function renderLicenseSection(license) {}
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+# Description
+${data.description}
+# Installation
+${data.intallation}
+# Usage
+${data.usage}
+# License
+${data.license}
+# Contribution
+${data.contributing}
+# Test
+${data.test}
+# Questions
+**For any questions please email me at - **${data.questions}
+# Github
+**See this project and others at - **${data.github}
+
 
 `;
 }
